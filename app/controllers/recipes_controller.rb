d1acb04c1a305222edbbf534d63bf9c58ff1b917
vagrant <vagrant@localhost.localdomain>
2020-09-05 18:18:35 +0000

    deploy setting

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,22 @@
+class RecipesController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+end
